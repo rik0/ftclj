25c1f9a1eaf5fb47ce8205aca278b11df4e46a82
Enrico Franchi <efranchi@ce.unipr.it>
2011-09-02 17:17:53 +0200

    Added some interesting functions: * tails * map * take-while

--- ftclj.clj
+++ ftclj.clj
@@ -1,5 +1,5 @@
 (ns ftclj
-  (:refer-clojure :exclude [map])
+  (:refer-clojure :exclude [map take-while])
   (:use clojure.test))
 
 (defn unfold
@@ -15,10 +15,31 @@
 (defn map [f xs]
   (unfold empty? #(f (first %)) rest xs))
 
+(defn take-while [pred coll]
+  (unfold pred first rest coll))
+
+(defn tails [xs]
+  (unfold empty? identity rest xs
+          #(list %)))
 
 (deftest test-map []
          (is (= (map first []) []))
          (is (= (map (fn [x] (* x 2)) (range 10))
                 '(0 2 4 6 8 10 12 14 16 18))))
+
+(deftest test-tails []
+         (letfn [(standard-tails [xs]
+                                 (cons xs
+                                       (when (seq xs)
+                                         (tails (rest xs)))))]
+           (is (= (tails []) (standard-tails [])))
+           (is (= (tails (range 4)) (standard-tails (range 4))))))
+
+(deftest test-take-while []
+         (let [c (range 10)]
+           (is (= (take-while (fn [x] (< x 5)) c)
+                  (clojure.core/take-while (fn [x] (< x 5)) c)))
+           (is (= (take-while (fn [x] (< x 0)) c)
+                  (clojure.core/take-while (fn [x] (< x 0)) c)))))
 
 (run-tests)
